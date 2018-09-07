7e9dc2e0135726287c8c1c432122e4dc0f55138f
Jeff Grant <jeffery.grant@gmail.com>
2018-09-07 17:09:23 -0400

    Split calibration module into separate classes

---
+++ mat/v2_calibration.py
@@ -0,0 +1,33 @@
+class V2Calibration(Calibration):
+    @classmethod
+    def load_from_string(cls, calibration_string):
+        coefficients = {}
+        for tag, value in cls._generate_tag_value_pairs(calibration_string):
+            coefficients[tag] = value
+        return cls(coefficients)
+
+    @staticmethod
+    def _generate_tag_value_pairs(calibration_string):
+        calibration_string = _trim_start(calibration_string, 3)
+        while True:
+            tag = calibration_string[0:3]
+            if tag == 'HSE':
+                break
+            length = int(calibration_string[3], 16)
+            value = calibration_string[4:4+length]
+            calibration_string = _trim_start(calibration_string, 4+length)
+            yield tag, value
+
+    def make_serial_string(self):
+        """
+        This generator function formats the host storage dict for writing
+        to the logger.
+        """
+        yield 'RVN12'
+        for key in self.coefficients:
+            if key == 'RVN':
+                continue
+            # TODO this may need to be changed to a number of decimal
+            value = str(self.coefficients[key])
+            length_hex = '%x' % len(value)
+            yield key + length_hex + value
---
+++ mat/v3_calibration.py
@@ -0,0 +1,32 @@
+class V3Calibration(Calibration):
+    def __init__(self, coefficients):
+        for tag in coefficients:
+            if 'RVN' in tag:
+                coefficients[tag] = float(coefficients[tag])
+                continue
+            coefficients[tag] = ascii85.ascii85_to_num(coefficients[tag])
+        super().__init__(coefficients)
+
+    @classmethod
+    def load_from_string(cls, calibration_string):
+        coefficients = {'RVN': 3}
+        calibration_string = calibration_string[8:]
+        tag = calibration_string[:3]
+        while tag != 'HSE':
+            value = calibration_string[3:8]
+            coefficients[tag] = value
+            calibration_string = calibration_string[8:]
+            tag = calibration_string[0:3]
+        return cls(coefficients)
+
+    def make_serial_string(self):
+        """
+        This generator function formats the host storage dict for writing
+        to the logger.
+        """
+        yield 'RVN13'
+        for key in self.coefficients:
+            if key == 'RVN':
+                continue
+            value = ascii85.num_to_ascii85(self.coefficients[key])
+            yield key + value
