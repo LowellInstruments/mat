ab5dd08dba2ad455396c21847857daf3d15cb15d
Jeff Grant <jeffery.grant@gmail.com>
2018-09-11 16:42:56 -0400

    Messing around with separating out a Sensor class. I implemented a named tuple in order to define the possible sensors. The class determines which are active based on the header. I have working a very nice way to build the sensor sequence based on the header and a sensor priority.

--- mat/sensor.py
+++ mat/sensor.py
@@ -1,62 +1,65 @@
-from abc import ABC, abstractmethod
-from mat.time_sequence import TimeSequence
-from mat.sensor_filter import SensorFilter
-import mat.converter as converter
+from collections import namedtuple
 
 
-class AbstractSensorFactory(ABC):
-    def __init__(self, header, calibration):
+SensorSpec = namedtuple('SensorSpec', [
+    'name',
+    'tag',
+    'priority',
+    'channels',
+    'interval_tag',
+    'burst_rate_tag',
+    'burst_count_tag',
+    'data_type']
+)
+
+AVAILABLE_SENSORS = [
+    SensorSpec('Temperature', 'TMP', 1, 1, 'TRI', None, None, 'uint16'),
+    SensorSpec('Pressure', 'PRS', 2, 1, 'ORI', 'PRR', 'PRN', 'uint16'),
+    SensorSpec('Light', 'PHD', 3, 1, 'TRI', None, None, 'uint16'),
+    SensorSpec('Accelerometer', 'ACL', 4, 3, 'ORI', 'BMR', 'BMN', 'int16'),
+    SensorSpec('Magnetometer', 'MGN', 5, 3, 'ORI', 'BMR', 'BMN', 'int16')
+]
+
+
+class SensorGroup:
+    def __init__(self, header):
         self.header = header
-        self.calibration = calibration
+        self.active_sensors = []
 
-    @abstractmethod
-    def make_time_sequence(self):
-        pass
+    def equip(self):
+        for spec in AVAILABLE_SENSORS:
+            if self.header.tag(spec.tag):
+                self.active_sensors.append(Sensor(spec, self.header))
+        self._verify_priority()
 
-    @abstractmethod
-    def make_sensor_filter(self):
-        pass
+    def data_sequence(self, page_seconds):
+        time_priority = []
+        for sensor in self.active_sensors:
+            time_offset = sensor.time_offset(page_seconds)
+            sensor_time_priority = [(t, sensor.priority) for t in time_offset]
+            time_priority.extend(sensor_time_priority)
+        return sorted(time_priority)
 
-    @abstractmethod
-    def make_calibration(self):
-        pass
-
-
-class AccelSensorFactory(AbstractSensorFactory):
-    def make_time_sequence(self):
-        interval = self.header['ORI']
-        frequency = self.header['BMR']
-        burst_length = self.header['BMN']
-        page_seconds = 1000  # 1000 is a placeholder for value TBD
-        return TimeSequence(interval, frequency, burst_length, page_seconds)
-
-    def make_sensor_filter(self):
-        burst_length = self.header['BMN']
-        channels = 3
-        data_type = 'int16'
-        return SensorFilter(burst_length, channels, data_type)
-
-    def make_calibration(self):
-        return converter.Accelerometer.factory(self.calibration)
-
-
-class MagSensorFactory(AccelSensorFactory):
-    def make_calibration(self):
-        return converter.Magnetometer.factory(self.calibration)
+    def _verify_priority(self):
+        priority = [spec.priority for spec in self.active_sensors]
+        if len(priority) != len(set(priority)):
+            raise ValueError('There cannot be more than one sensor with '
+                             'the same priority')
 
 
 class Sensor:
-    def __init__(self, header, calibration):
+    def __init__(self, spec, header):
+        self.name = spec.name
+        self.priority = spec.priority
+        self.channels = spec.channels
+        self.interval = header.tag(spec.interval_tag)
+        self.burst_rate = header.tag(spec.burst_rate_tag) or 1
+        self.burst_count = header.tag(spec.burst_count_tag) or 1
+        self.data_type = spec.data_type
 
-
-################
-self.active_sensors = []
-if 'TMP' in self.header and self.header['TMP'] == 1:
-    pass
-
-if 'ACL' in self.header and self.header['ACL'] == 1:
-    self.active_sensors.append(AccelSensorFactory(header, calibration))
-
-if 'MGN' in self.header and self.header['MGN'] == 1:
-    self.active_sensors.append(MagSensorFactory(header, calibration))
-
+    def time_offset(self, seconds):
+        time_offset = []
+        for interval_time in range(0, seconds, self.interval):
+            for burst_time in range(0, self.burst_count):
+                time_offset.append(interval_time + burst_time / self.burst_rate)
+        return time_offset
