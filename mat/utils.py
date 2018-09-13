b1f5eacc05fca71dc7b65855a3fe72afbbc7e0cb
Nathan Wilson <nathan@collectivesource.com>
2018-09-13 15:30:56 -0400

    Replace _trim_start with trim_start, array_from_tags returns array

--- mat/utils.py
+++ mat/utils.py
@@ -1,3 +1,6 @@
+from numpy import array
+
+
 def obj_from_coefficients(coefficients, classes):
     coefficient_set = set(coefficients)
     for klass in classes:
@@ -7,10 +10,10 @@ def obj_from_coefficients(coefficients, classes):
     return None
 
 
-def _trim_start(string, n_chars_to_trim):
+def trim_start(string, n_chars_to_trim):
     return string[n_chars_to_trim:]
 
 
 def array_from_tags(data, *key_lists):
-    return [[data[key] for key in key_list]
-            for key_list in key_lists]
+    return array([[data[key] for key in key_list]
+                  for key_list in key_lists])
