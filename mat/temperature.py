451403e4cc6179f5f9b25b0647eb8dff9191a8ce
Nathan Wilson <nathan@collectivesource.com>
2018-09-13 14:37:21 -0400

    Remove magic constants

--- mat/temperature.py
+++ mat/temperature.py
@@ -1,6 +1,10 @@
 from numpy import log
 
 
+ZERO_KELVIN = -273.15
+MAX_16_BIT_INT = 65535
+
+
 class Temperature:
     def __init__(self, calibration):
         coefficients = calibration.coefficients
@@ -10,7 +14,7 @@ class Temperature:
         self.tmr = coefficients['TMR']
 
     def convert(self, raw_temperature):
-        temp = (raw_temperature * self.tmr) / (65535 - raw_temperature)
+        temp = (raw_temperature * self.tmr) / (MAX_16_BIT_INT - raw_temperature)
         return 1 / (self.tma +
                     self.tmb * log(temp) +
-                    self.tmc * (log(temp)) ** 3) - 273.15
+                    self.tmc * (log(temp)) ** 3) + ZERO_KELVIN
