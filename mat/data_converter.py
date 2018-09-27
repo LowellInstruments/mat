1d361fb90aa8edb9df85f8e260409056e61fb8b1
Jeff Grant <jeffery.grant@gmail.com>
2018-09-27 14:59:28 -0400

    Added ability to cancel conversion (to work with Converter)

--- mat/data_converter.py
+++ mat/data_converter.py
@@ -17,6 +17,7 @@ class DataConverter:
         self.parameters.update({'path': path})
         self._source_file = None
         self.observers = []
+        self._is_running = None
 
     def source_file(self):
         if self._source_file:
@@ -24,11 +25,17 @@ class DataConverter:
         self._source_file = load_data_file(self.path)
         return self._source_file
 
+    def cancel_conversion(self):
+        self._is_running = False
+
     def convert(self):
+        self._is_running = True
         outputs = self.get_outputs(self.source_file().sensors(),
                                    self.parameters)
         page_times = self.source_file().page_times()
         for i in range(self.source_file().n_pages()):
+            if not self._is_running:
+                break
             page = self.source_file().page(i)
             for this_output in outputs:
                 this_output.process_page(page, page_times[i])
--- mat/data_file_registry.py
+++
@@ -1,6 +0,0 @@
-class DataFileRegistry:
-    registry = {}
-
-    @classmethod
-    def register(cls, klass):
-        cls.registry[klass.extension] = klass
