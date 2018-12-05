84e0c2594a5e8e38b0c35dee9e5423c7d4c8a86b
kazola <joaquim.oller@gmail.com>
2018-12-05 11:30:02 -0500

    GPS branch ready to merge.

--- mat/gps.py
+++ mat/gps.py
@@ -12,7 +12,6 @@ def parse_float(x):
     return float(x) if x else None
 
 
-# Device Object
 class GPS:
 
     BAUD_RATE_BU_353_S4 = 4800
@@ -20,56 +19,45 @@ class GPS:
                            'valid timestamp latitude longitude knots course')
 
     def __init__(self, port, baud_rate=BAUD_RATE_BU_353_S4):
-        self.port = serial.Serial(port, baud_rate)
+        self.port = serial.Serial(port, baud_rate, timeout=1)
         self.handlers = {
             '$GPRMC': self._on_rmc,
         }
-        self.rmc = None
+        self.last_rmc = None
 
-    # def read_line(self, timeout=5):
-    #     end_time = time.time() + timeout
-    #     while time.time() < end_time:
-    #         time.sleep(0.1)
-    #         line_bytes = self.port.readline().strip()
-    #         if line_bytes.startswith('$'.encode('ASCII')):
-    #             line = line_bytes.decode('ASCII')
-    #             data, checksum = line.split('*')
-    #             tokens = data.split(',')
-    #             command, args = tokens[0], tokens[1:]
-    #             # sent to parsers, aka handlers, only '_on_rmc_()' here
-    #             handler = self.handlers.get(command)
-    #             if GPS._verify_string(data, checksum) and handler:
-    #                 handler(args)
-    #                 return line
-    #     return None
-
-    def read_line(self, timeout=5):
+    # waits for frame_type + checksum OK -> populates self.last_* attributes
+    def wait_for_frame_type(self, frame_type, timeout=3):
         end_time = time.time() + timeout
         while time.time() < end_time:
-            time.sleep(0.1)
             line_bytes = self.port.readline().strip()
-            parsed = self._parse_line(line_bytes)
-            if parsed:
-                return parsed
+            frame_type_received = self._parse_line(line_bytes, frame_type)
+            if frame_type_received:
+                return frame_type_received
+            else:
+                time.sleep(0.1)
         return None
 
-    def _parse_line(self, line_bytes):
+    def _parse_line(self, line_bytes, frame_type):
         if line_bytes.startswith('$'.encode('ASCII')):
             line = line_bytes.decode('ASCII')
             data, checksum = line.split('*')
             tokens = data.split(',')
-            command, args = tokens[0], tokens[1:]
-            # sent to parsers, aka handlers, only '_on_rmc_()' here
-            handler = self.handlers.get(command)
-            if GPS._verify_string(data, checksum) and handler:
-                handler(args)
-                return line
+            gps_sentence, args = tokens[0], tokens[1:]
+            if not GPS._verify_string(data, checksum):
+                return None
+            if gps_sentence != frame_type:
+                return None
+            handler = self.handlers.get(gps_sentence)
+            if handler is None:
+                return None
+            handler(args)
+            return line
+        else:
             return None
-        return None
 
     @classmethod
     def _verify_string(cls, data, checksum):
-        # get rid of '$' character
+        # lose '$' character at the beginning of gps sentence
         data = data[1:]
         checksum_in_decimal = int(checksum, 16)
         int_values = [ord(x) for x in data]
@@ -86,7 +74,7 @@ class GPS:
         longitude = GPS._to_decimal_degrees(args[4], args[5])
         knots = parse_float(args[6])
         course = parse_float(args[7])
-        self.rmc = GPS.RMC_Frame(
+        self.last_rmc = GPS.RMC_Frame(
             valid=valid,
             timestamp=timestamp,
             latitude=latitude,
@@ -97,7 +85,7 @@ class GPS:
 
     @classmethod
     def _to_decimal_degrees(cls, value, nsew):
-        # BU-353-S4 GPS provides RMC frame as DDMM.mmmm
+        # BU-353-S4 latitude, longitude fields are DDMM.mmmm but not always filled
         if not value:
             return None
         a, b = value.split('.')
@@ -112,6 +100,6 @@ class GPS:
         return result
 
     def get_last_rmc_frame(self):
-        if self.rmc is not None and self.rmc.timestamp:
-            return self.rmc
+        if self.last_rmc and self.last_rmc.timestamp:
+            return self.last_rmc
         return {}
--- tests/test_gps.py
+++ tests/test_gps.py
@@ -9,7 +9,7 @@ from unittest import TestCase
 
 
 class FakeSerial:
-    def __init__(self, fakeport, fakebaudrate):
+    def __init__(self, fakeport, fakebaudrate, timeout=1):
         pass
 
     def readline(self):
@@ -23,6 +23,12 @@ class FakeSerialWrongChecksum(FakeSerial):
         07038.2859,W,0.00,196.76,041218,,,A*11'
 
 
+class FakeSerialReadlineNotStartsWithDollar(FakeSerial):
+    def readline(self):
+        return b'GPRMC,210239.000,A,4134.2946,N,\
+        07038.2859,W,0.00,196.76,041218,,,A*11'
+
+
 @contextmanager
 def _patch_serial(fake_serial_class):
     with patch('serial.Serial', fake_serial_class):
@@ -30,33 +36,30 @@ def _patch_serial(fake_serial_class):
 
 
 class TestGPS(TestCase):
-    # check: http://www.hiddenvision.co.uk/ez/?nmea_lat=3015.4550S&nmea_lon=
-    def test_convert_lat_n(self):
+    # link: https://www.directionsmag.com/site/latlong-converter/
+    def test_convert_lat_n_to_decimal_degrees(self):
         lat_str = '3015.4550'
         assert GPS._to_decimal_degrees(lat_str, 'N') == 30.257583333333333
 
-    def test_convert_lat_s(self):
+    def test_convert_lat_s_to_decimal_degrees(self):
         lat_str = '3015.4550'
         assert GPS._to_decimal_degrees(lat_str, 'S') == -30.257583333333333
 
-    def test_convert_lon_e(self):
+    def test_convert_lon_e_to_decimal_degrees(self):
         lon_str = '07301.3281'
         assert GPS._to_decimal_degrees(lon_str, 'E') == 73.022135
 
-    def test_convert_lon_w(self):
+    def test_convert_lon_w_to_decimal_degrees(self):
         lon_str = '07301.3281'
         assert GPS._to_decimal_degrees(lon_str, 'W') == -73.022135
 
-    def test_convert_lon_too_short(self):
+    def test_convert_lon_too_short_to_decimal_degrees(self):
         lon_str = '807.2500'
         with self.assertRaises(ValueError):
             GPS._to_decimal_degrees(lon_str, 'W')
 
-    def test_to_decimal_value_none(self):
+    def test_convert_none_to_decimal_degrees(self):
         assert GPS._to_decimal_degrees('', 'SW') is None
-
-    def test_to_decimal_value_existing(self):
-        assert GPS._to_decimal_degrees('4119.6607', 'SW') == -41.32767833333333
 
     def test_parse_int(self):
         assert parse_int('3') == 3
@@ -64,29 +67,38 @@ class TestGPS(TestCase):
     def test_parse_float(self):
         assert parse_float('3.5') == 3.5
 
-    def test_gps_read_line(self):
+    def test_gps_wait_for_rmc_frame_type(self):
+        with _patch_serial(FakeSerial):
+            assert GPS('any', 115200).wait_for_frame_type('$GPRMC') is not None
+
+    def test_gps_wait_for_not_rmc_frame_type(self):
+        with _patch_serial(FakeSerial):
+            assert GPS('any', 115200).wait_for_frame_type('$GPXXX') is None
+
+    def test_gps_wait_for_rmc_frame_type_no_handler(self):
         with _patch_serial(FakeSerial):
             o = GPS('any', 115200)
-            frame = o.read_line()
-            assert frame is not None
+            o.handlers = {}
+            assert o.wait_for_frame_type('$GPRMC') is None
 
-    def test_gps_read_line_wrong_checksum(self):
+    def test_gps_wait_for_rmc_frame_type_checksum_bad_and_timeouts(self):
         with _patch_serial(FakeSerialWrongChecksum):
-            o = GPS('any', 115200)
-            frame = o.read_line()
-            assert frame is None
+            assert GPS('any', 115200).wait_for_frame_type('$GPRMC') is None
 
-    def test_my_measures(self):
+    def test_gps_wait_for_rmc_frame_type_but_not_starts_with_dollar(self):
+        with _patch_serial(FakeSerialReadlineNotStartsWithDollar):
+            assert GPS('any', 115200).wait_for_frame_type('$GPRMC') is None
+
+    def test_get_last_rmc_frame_not_empty(self):
         with _patch_serial(FakeSerial):
-            last_rmc = GPS.RMC_Frame(True, '2018-11-21 18:21:52', 42.0003,
-                                     69.9835, 0.56, 190.22)
             o = GPS('any', 115200)
-            o.rmc = last_rmc
+            o.last_rmc = GPS.RMC_Frame(True, '2018-11-21 18:21:52', 42.0003,
+                                       69.9835, 0.56, 190.22)
             expected = GPS.RMC_Frame(True, '2018-11-21 18:21:52', 42.0003,
                                      69.9835, 0.56, 190.22)
             assert o.get_last_rmc_frame() == expected
 
-    def test_last_rmc_frame(self):
+    def test_get_last_rmc_frame_empty(self):
         with _patch_serial(FakeSerial):
             o = GPS('any', 115200)
             assert o.get_last_rmc_frame() == {}
