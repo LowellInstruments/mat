d2b36ec785b8797e5ee68929e51b4094df6754dc
jeff_grant <jeffery.grant@gmail.com>
2019-07-27 11:15:25 -0400

    fixes a code check issue

--- mat/dissolved_oxygen.py
+++ mat/dissolved_oxygen.py
@@ -4,9 +4,9 @@ from mat.meter import Meter
 
 """
 Dissolved oxygen are read from the sensor in engineering units (no calibration
-or conversion required). The values are written to the .lid file as 2 byte 
-unsigned ints in a modified BCD format (xx.xx) where there is a decimal 
-between the first and last two digits.  
+or conversion required). The values are written to the .lid file as 2 byte
+unsigned ints in a modified BCD format (xx.xx) where there is a decimal
+between the first and last two digits.
 e.g. an int value of 9847 is 0x2677 which yields a final value of 26.77
 """
 
