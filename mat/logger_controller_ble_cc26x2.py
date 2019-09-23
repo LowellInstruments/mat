3fa10d0a6f94549a47afb735b9018eff32c0e07f
kazola <kaz@kaz.com>
2019-09-23 13:53:39 -0400

    added time for update conn parameters req

--- mat/logger_controller_ble_cc26x2.py
+++ mat/logger_controller_ble_cc26x2.py
@@ -16,6 +16,11 @@ class LoggerControllerBLECC26X2(LoggerControllerBLE):
         # set_mtu() needs some time (bluepy issue 325)
         self.peripheral.setMTU(self.MTU_SIZE)
         time.sleep(1)
+
+        # extra sleep to receive connection update request from cc26x2
+        time.sleep(1)
+
+        # this is where we write to when talking to cc26x2 loggers
         self.cha = self.svc.getCharacteristics(self.UUID_W)[0]
 
     def ble_write(self, data, response=False):  # pragma: no cover
@@ -26,7 +31,7 @@ class LoggerControllerBLECC26X2(LoggerControllerBLE):
         cfg_file_as_string = json.dumps(cfg_file_as_json_dict)
         return self.command("CFG", cfg_file_as_string, retries=1)
 
-    # bat function, only ccs26x2
+    # bat function, only cc26x2
     def get_bat(self):
         self.delegate.clear_delegate_buffer()
         return self.command('BAT')
