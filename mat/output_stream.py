f7dd5a405f3323d52d81d07f506c0f34c512f557
Jeff Grant <jeffery.grant@gmail.com>
2019-10-03 11:01:07 -0400

    adds HDF5 output stream

--- mat/output_stream.py
+++ mat/output_stream.py
@@ -1,9 +1,12 @@
 from os import path
 from .time_converter import create_time_converter
+from pathlib import Path
+import h5py
+import numpy as np
 
 
 def output_stream_factory(file_path, parameters):
-    output_types = {'csv': CsvStream}
+    output_types = {'csv': CsvStream, 'hdf5': HDF5Stream}
     stream_class = output_types.get(parameters['output_format'])
     if stream_class is None:
         raise ValueError('Unknown output type' + parameters['output_format'])
@@ -40,14 +43,65 @@ class OutputStream:
 
 class CsvStream(OutputStream):
     def add_stream(self, data_product):
-        self.streams[data_product] = CsvFile(self.file_path,
-                                             data_product,
-                                             self.parameters)
+        self.streams[data_product] = CsvFile(
+            self.file_path, data_product, self.parameters
+        )
+
+
+class HDF5Stream(OutputStream):
+    hdf_file = None
+
+    def __init__(self, file_path, parameters):
+        super().__init__(file_path, parameters)
+        self.time_converter = create_time_converter('iso8601')
+        if HDF5Stream.hdf_file:
+            return
+        path = Path(file_path)
+        hdf_path = (path.parent / path.stem).with_suffix('.hdf5')
+        if hdf_path.exists() and not self.overwrite:
+            raise FileExistsError(str(path.name))
+        HDF5Stream.hdf_file = h5py.File(hdf_path, 'w')
+
+    def file(self):
+        return HDF5Stream.hdf_file
+
+    def add_stream(self, data_product):
+        self.streams[data_product] = self.file().create_group(data_product)
+
+    def set_column_header(self, stream, column_header):
+        self.streams[stream].create_dataset(
+            'Time', (0, ),
+            maxshape=(None, ),
+            dtype='S23',
+            compression='gzip',
+            shuffle=True
+        )
+        self.streams[stream]['Time'].attrs['Columns'] = 'ISO 8601 Time'
+
+        channels = column_header.split(',')
+        self.streams[stream].create_dataset(
+            'Data',
+            (0, len(channels)),
+            maxshape=(None, len(channels)),
+            compression='gzip',
+            shuffle=True
+        )
+        self.streams[stream]['Data'].attrs['Columns'] = column_header
+
+    def write(self, stream, data, time):
+        ds_data = self.streams[stream]['Data']
+        ds_time = self.streams[stream]['Time']
+
+        ds_shape = ds_data.shape
+        new_length = ds_shape[0] + data.shape[1]
+
+        ds_data.resize((new_length, ds_shape[1]))
+        ds_time.resize((new_length, ))
+        ds_data[ds_shape[0]:, :] = data.T
+        ds_time[ds_shape[0]:] = np.string_(self.time_converter.convert(time))
 
 
 class CsvFile:
-    EXTENSION = '.csv'
-
     def __init__(self, file_path, stream_name, parameters):
         self.file_path = file_path
         self.stream_name = stream_name
@@ -90,10 +144,3 @@ class CsvFile:
     def _write_header(self):
         with open(self.output_path, 'w') as fid:
             fid.write(self.column_header + '\n')
-
-
-class HdfFile(OutputStream):
-    """
-    Create a single file with each stream as a separate data set
-    """
-    pass
