bfbb5424b7c87905ee9128fa953e52ddb3517a3b
kaz <kaz>
2020-12-02 19:29:02 -0500

    agents!

--- mat/agent_ble.py
+++ mat/agent_ble.py
@@ -1,7 +1,7 @@
 import threading
 import time
 
-import pytest
+from mat.logger_controller import STOP_CMD, SET_TIME_CMD, STATUS_CMD
 from mat.logger_controller_ble import LoggerControllerBLE
 
 
@@ -52,11 +52,116 @@ class AgentBLE(threading.Thread):
             return 0, rv
         return 1, rv
 
+    def set_time(self, mac):
+        # ------------ this crashes because we need argument time :)
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        rv = self.lc.command(SET_TIME_CMD)
+        print(rv)
+        if rv == [b'STM', b'00']:
+            return 0, rv
+        return 1, rv
+
+    def status(self, mac):
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        rv = self.lc.command(STATUS_CMD)
+        if rv[0] == b'STS' and len(rv[1]) == 4:
+            return 0, rv
+        return 1, rv
+
+    def ls_lid(self, mac):
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        # todo: return my 0 and 1 here and below not_lid()
+        return self.lc.ls_lid()
+
+    def ls_not_lid(self, mac):
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        return self.lc.ls_not_lid()
+
+    def stop_logger(self, mac):
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        rv = self.lc.command(STOP_CMD)
+        if rv == [b'STP', b'00']:
+            return 0, 'logger stopped'
+        return 1, 'logger not stopped'
+
+    def get_file(self, mac, file, fol, size):
+        rv = self.connect(mac)
+        if rv[0] == 1:
+            return rv
+        # todo: pass sig as parameter
+        rv = self.lc.get_file(file, fol, size)
+        if rv:
+            return 0, 'file {} size {}'.format(file, size)
+        return 1, 'err get_file {} size {}'.format(file, 0)
+
     def close(self):
         return self.disconnect()
 
 
 class TestBLEAgent:
+    def test_status(self):
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        rv = th.status(mac)
+        assert rv[0] == 0
+
+    def test_set_time(self):
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        rv = th.set_time(mac)
+        assert rv[0] == 0
+
+    def test_get_file(self):
+        # this may not output text during a long time
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        file = '2006671_low_20201004_132205.lid'
+        size = 299950
+        rv = th.get_file(mac, file, '.', size)
+        assert rv[0] == 0
+
+    def test_stop(self):
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        rv = th.stop_logger(mac)
+        assert rv[0] == 0
+
+    def test_ls_lid(self):
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        rv = th.ls_lid(mac)
+        _p(rv)
+        assert type(rv) == dict
+
+    def test_ls_not_lid(self):
+        th = AgentBLE()
+        th.start()
+        th.disconnect()
+        mac = '60:77:71:22:C8:18'
+        rv = th.ls_not_lid(mac)
+        _p(rv)
+        assert type(rv) == dict
+
     def test_get_time(self):
         # sending commands implicitly connects
         th = AgentBLE()
