c164e6863d155e9ea193c8e898bfd0b27a465965
kaz <kaz>
2022-10-31 19:48:14 -0400

    more

--- mat/aws/sqs.py
+++ mat/aws/sqs.py
@@ -1,6 +1,8 @@
 import os
+import random
 
 from botocore.exceptions import ClientError
+import uuid
 
 
 def sqs_get_queue(q_name, session_sqs):
@@ -21,12 +23,20 @@ def sqs_get_queue(q_name, session_sqs):
 
 def sqs_enqueue_msg(queue, message_body, message_attributes=None):
     if not message_attributes:
-        message_attributes = {}
+        message_attributes = {
+            'my_rand': {
+                'StringValue': str(uuid.uuid4()),
+                'DataType': 'String'
+            }
+        }
 
     try:
         response = queue.send_message(
             MessageBody=message_body,
-            MessageAttributes=message_attributes
+            MessageAttributes=message_attributes,
+            # todo ---> research but seems to work to prevent SQS filtering our messages
+            MessageGroupId=str(uuid.uuid4()),
+            MessageDeduplicationId=str(uuid.uuid4())
         )
 
     except ClientError as error:
