c4d40d320b09a7fb50bc53670c5b976ebf8ec3ab
kazola <kazola>
2023-01-27 16:55:02 -0500

    more

--- settings/installer_lowell_mat.sh
+++ settings/installer_lowell_mat.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/bash
+echo; echo; echo
 
+
+# -----------------------------------------------------------------------
+# this script automatically installs lowell-mat without its dependencies
 # call as:
-# curl https://raw.githubusercontent.com/LowellInstruments/liu/master/liu/installer_liu.sh | bash
+# curl https://raw.githubusercontent.com/LowellInstruments/lowell-mat/v4/settings/installer_lowell_mat.sh | bash
+# -----------------------------------------------------------------------
 
-clear
-echo 'hello'
-echo
+
+printf '> CURL installer for Lowell Instruments MAT library\n'
+printf '> Cloning repository lowell-mat'
+git clone https://github.com/lowellinstruments/lowell-mat.git -b v4
+rv=$?
+if [ "$rv" -ne 0 ]; then
+    printf 'error cloning lowell-mat github repository'
+    exit 1
+fi
+
+
+printf '> Installing lowell-mat in empty-dependencies mode'
+cd lowell-mat && export MY_IGNORE_REQUIREMENTS_TXT=1 && pip install . -v
+rv=$?
+if [ "$rv" -ne 0 ]; then
+    printf 'error installing lowell-mat'
+    exit 1
+fi
