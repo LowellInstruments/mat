1e2b4d4e6b780e7314026f070836cb2b621db67f
kaz <kaz>
2023-02-04 01:03:30 -0500

    setup_stuff

---
+++ tools/_setup_wo_reqs.py
@@ -0,0 +1,53 @@
+# GPLv3 License
+# Copyright (c) 2018 Lowell Instruments, LLC, some rights reserved
+
+
+from setuptools import setup, find_packages
+import os
+
+
+# =============================================
+# this file is used by DDH automated installer
+# =============================================
+
+
+# obtain package version from local file
+v = {}
+with open("mat/version.py") as f:
+    exec(f.read(), v)
+
+
+setup(name='lowell-mat',
+      version=v['__version__'],
+      description='Shared package for Lowell Instruments software',
+      url='https://github.com/LowellInstruments/lowell-mat',
+      author='Lowell Instruments',
+      author_email='software@lowellinstruments.com',
+      packages=find_packages(),
+
+      # -------------------------------------------------
+      # allows MAT library to be installed empty of reqs
+      # -------------------------------------------------
+      install_requires=[
+      ],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: MacOS X",
+          "Environment :: Win32 (MS Windows)",
+          "Environment :: X11 Applications",
+          "Environment :: X11 Applications :: Qt",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Natural Language :: English",
+          "Operating System :: MacOS",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: Microsoft :: Windows :: Windows 10",
+          "Operating System :: Microsoft :: Windows :: Windows 7",
+          "Operating System :: Microsoft :: Windows :: Windows 8",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+      ])
