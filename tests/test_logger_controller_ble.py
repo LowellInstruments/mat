5969c4e86b702c60fbc442157f0dacf737874af2
kaz <kaz>
2023-03-18 21:51:41 -0400

    github

--- tests/test_logger_controller_ble.py
+++ tests/test_logger_controller_ble.py
@@ -1,8 +1,15 @@
+import os
+
+
 class TestLoggerControllerBle:
     def test_all_definitions(self):
-        with open('../mat/logger_controller_ble.py') as f:
+        # local tests
+        path = '../mat/logger_controller_ble.py'
+        if os.getenv('GITHUB_ACTIONS'):
+            path = 'mat/logger_controller_ble.py'
+        with open(path) as f:
             ll = f.readlines()
             for i in ll:
                 if '=' not in i:
                     continue
-                assert '' not in i
+                assert "''" not in i
