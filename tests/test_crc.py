faa6c7c53b2edb8e6939e876fb5d92a5e6467c88
kaz <kaz>
2023-03-18 22:20:49 -0400

    more

--- tests/test_crc.py
+++ tests/test_crc.py
@@ -5,6 +5,6 @@ from mat.crc import calculate_local_file_crc
 class TestMatCRC:
     def test_mat_crc(self):
         path_local = 'files/bad.lid'
-        path_abs = 'mat/tests/files/bad.lid'
+        path_abs = 'tests/files/bad.lid'
         path = path_abs if os.getenv('GITHUB_ACTIONS') else path_local
         assert calculate_local_file_crc(path) == '4a745ad9'
