09ab6dd100688aa70b3851b13ea5c6e7596662e7
kaz <kaz>
2023-04-06 18:54:20 -0400

    water

--- mat/water_detect.py
+++ mat/water_detect.py
@@ -1,13 +1,9 @@
-import math
-
 
 class WaterDetect:
     def __init__(self, hs=None):
         pass
 
-    def convert(self, mV):
+    def convert(self, mv):
         # 100% water is 3000 mV == VCC
         vcc = 3000
-
-        # prevent 100% in data file
-        return math.floor((mV / vcc) * 100)
+        return ((mv / vcc) * 100).round()
