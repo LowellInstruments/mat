3cf4a85c245322f6a4896c20316b2e6cb2fca0a6
kaz <kaz>
2024-01-21 19:03:56 -0500

    lix file

--- mat/lix.py
+++ mat/lix.py
@@ -16,21 +16,17 @@ class ParserLixFile:
         self.bb = bytes()
         # sm: sensor mask
         self.sm = None
-        # i: index bytes
-        self.i = 0
         # named tuple macro-header
         self.mah = namedtuple("MacroHeader",
                               "logger_type "
                               "sm")
         # dictionary measurements
-        self.d_measurements = None
+        self.d_measurements = dict()
         # dictionary micro-headers
-        self.d_mih = None
+        self.d_mih = dict()
 
     def _parse_macro_header(self):
         mah = self.bb[:CS]
-        # update index
-        self.i += CS
 
     def parse_lix_file(self, p):
         self.path = p
@@ -39,6 +35,44 @@ class ParserLixFile:
         self._parse_macro_header()
         assert self.sm
         self._parse_data()
+
+    def _parse_data_mih(self, mi, i):
+        # 2B battery, 1B header index % 255,
+        # 1B ECL, 4B epoch
+        bat = mi[:i+2]
+        idx = mi[i+2]
+        ecl = mi[i+3]
+        t = mi[i+4:i+8]
+        self.d_mih[t] = 'bat, idx, ECL'
+
+    def _parse_data_measurement(self, mm, i):
+        mk = (mm[i] & 0xc0) >> 6
+        if mk == 0:
+            # no sensor mask, time simple
+            t = mm[i] & 0x3F
+            i += 1
+        elif mk == 1:
+            # no sensor mask, time extended
+            t = (mm[i] & 0x3F) << 8
+            t += mm[i+1]
+            i += 2
+        elif mk == 2:
+            # yes sensor mask, time simple
+            self.sm = mm[i] & 0x3F
+            t = mm[i+1]
+            i += 2
+        else:
+            # yes sensor mask, time extended
+            self.sm = mm[i] & 0x3F
+            t = (mm[i] & 0x3F) << 8
+            t += mm[i+1]
+            i += 3
+
+        # get measurement length from sensor mask
+        n = 6
+        self.d_measurements[t] = 'sensor1, sensor2, sensor3'
+        i += n
+        return i
 
     def _parse_data(self):
         if debug:
@@ -58,21 +92,22 @@ class ParserLixFile:
             measurements += data[i+UHS:i+CS]
             micro_headers += data[i:i+UHS]
 
-        print(len(measurements), measurements)
-        print(len(micro_headers), micro_headers)
-
         # build dictionary measurements
-        self.d_measurements = dict()
+        i = 0
         while 1:
-            # measurements are of variable length
-            break
+            try:
+                # measurements have variable length
+                i += self._parse_data_measurement(measurements, i)
+            except IndexError:
+                break
 
         # build dictionary micro_headers
-        self.d_mih = dict()
         for i in range(0, UHS, len(micro_headers)):
-            pass
+            self._parse_data_mih(micro_headers, i)
+
+        # maybe fusion both dictionaries here
 
 
 if __name__ == '__main__':
-    p = ParserLixFile()
-    p._parse_data()
+    plf = ParserLixFile()
+    plf._parse_data()
