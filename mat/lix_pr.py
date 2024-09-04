e825276904e0cf7cbb6f6b7d57eff62d26588aba
kaz <kaz>
2024-09-04 12:06:14 -0400

    toml

--- mat/lix_pr.py
+++ mat/lix_pr.py
@@ -40,6 +40,6 @@ def convert_lix_file(fp, more_columns=0, verbose=0):
 
 
 if __name__ == '__main__':
-    filename = '/home/kaz/Downloads/3333333_low_20240827_110323.lid'
+    filename = '/home/kaz/Downloads/2405500_TST_20240904_154902.lid'
     rv = convert_lix_file(filename, more_columns=1, verbose=1)
     print('rv', rv)
