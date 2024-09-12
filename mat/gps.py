f621ef4fc6722b83215f57a690797a9b6c704bab
kaz <kaz>
2024-09-12 09:22:35 -0400

    more

--- mat/gps.py
+++
@@ -1,4 +0,0 @@
-
-# hardcoded, since they are FIXED on SixFab hats
-PORT_CTRL = '/dev/ttyUSB2'
-PORT_DATA = '/dev/ttyUSB1'
---
+++ mat/quectel.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+
+# ------------------------------------
+# main Quectel USB: detects which USB
+# ports are being used by cell shield
+# ------------------------------------
+
+
+import serial
+import time
+
+
+SERIAL_RATE = 115200
+FILE_QUECTEL_USB_GPS = '/tmp/usb_quectel_gps'
+FILE_QUECTEL_USB_CTL = '/tmp/usb_quectel_ctl'
+
+
+def detect_quectel_usb_ports():
+    for i in (FILE_QUECTEL,USB_GPS, FILE_QUECTEL_USB_CTL):
+        if os.path.exists(i):
+            os.unlink(i)
+    found_gps = ''
+    found_ctl = ''
+    for i in range(5):
+        p = f'/dev/ttyUSB{i}'
+        till = time.perf_counter() + 1
+        b = bytes()
+        ser = None
+        try:
+            ser = serial.Serial(p, SERIAL_RATE, timeout=.1, rtscts=True, dsrdtr=True)
+            ser.write(b'AT+CVERSION \rAT+CVERSION \r')
+            while time.perf_counter() < till:
+                b += ser.read()
+                if b'$GPGSV' in b or b'$GPGSA' in b or b'GPRMC' in b:
+                    found_gps = p
+                    break
+                if b'VERSION' in b:
+                    found_ctl = p
+                    break
+            ser.close()
+            if found_gps and found_ctl:
+                break
+        except (Exception,) as ex:
+            if ser and ser.isOpen():
+                ser.close()
+            # print(f'error {p} -> {ex}')
+    with open(FILE_QUECTEL_USB_GPS, 'w') as f:
+        f.write(found_gps)
+    with open(FILE_QUECTEL_USB_CTL, 'w') as f:
+        f.write(found_ctl)
+    return found_gps, found_ctl
+
+
+if __name__ == '__main__':
+    rv = detect_quectel_usb_ports()
+    print('rv', rv)
+
+
